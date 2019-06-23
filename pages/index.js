e5dd4d80cc551f26342ebaf2f7afd2f004bb78bc
Ihsan Satriawan <ihsan.satriawan.20@gmail.com>
2019-06-24 06:09:43 +0700

    Layout with sidebar and content (#2)

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
+import { Carousel } from 'antd';
 import Link from 'next/link';
+import '../static/css/carousel.css';
+
+const CarouselTemplate = () => {
+  return (
+    <Carousel autoplay>
+      <div>
+        <h3>1</h3>
+      </div>
+      <div>
+        <h3>2</h3>
+      </div>
+      <div>
+        <h3>3</h3>
+      </div>
+      <div>
+        <h3>4</h3>
+      </div>
+    </Carousel>
+  )
+}
 
 const Index = () => (
   <div>
-    <Link href="/about">
-      <p>about</p>
-    </Link>
-    <Link href="/post?slug=something" as="/post/something"><p>hai</p></Link>
-    <p>Hello Next.js</p>
+    <CarouselTemplate />
   </div>
 );
 
