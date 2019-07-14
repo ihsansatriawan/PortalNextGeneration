1d033732fa19113f466f2dbfe042f59d7da8c245
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2019-07-14 22:37:50 +0700

    Add condition in Thanks page

--- components/FIM21/Thank.js
+++ components/FIM21/Thank.js
@@ -8,22 +8,40 @@ import {
   Result,
   Button,
 } from 'antd';
+import Router from 'next/router';
 
 class Thank extends Component {
+
+  state = {
+    isCanMultiple: false
+  }
+
+  componentDidMount = () => {
+    //TODO: Fetch data for update isCanMultiple
+  }
+
   render() {
     const { onChangeStep } = this.props
-    
+    const { isCanMultiple } = this.state
+
+    let extraButton = [
+      <Button onClick={() => { Router.push('/') }} type="primary" key="console">
+        Kembali Ke Beranda
+      </Button>
+    ]
+
+    if (isCanMultiple) {
+      extraButton.push(
+        <Button onClick={() => { onChangeStep(2) }} key="buy">Memilih Jalur Lain</Button>
+      )
+    }
+
     return (
       <Result
         status="success"
         title="Data Kamu sudah masuk, silahkan tunggu!"
-        subTitle="Silahkan Menunggu Pengumuman"
-        extra={[
-          <Button type="primary" key="console">
-            Kembali Ke Beranda
-          </Button>,
-          <Button onClick={() => { onChangeStep(2) }} key="buy">Memilih Jalur Lain</Button>,
-        ]}
+        subTitle={isCanMultiple ? "untuk kamu alumni FIM 20, dapat memilih jalur Next Gen dan jalur Non Next Gen sekaligus, dengan total maksimal 2 jalur yang didaftarkan" : "Silahkan Menunggu Pengumuman"}
+        extra={extraButton}
       />
     )
   }
