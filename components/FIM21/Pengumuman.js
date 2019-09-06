be00185f30d79456489ff1b3b9ab8bc44e3500e3
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2019-09-07 06:26:48 +0700

    Show lolos

--- components/FIM21/Pengumuman.js
+++ components/FIM21/Pengumuman.js
@@ -4,7 +4,7 @@ import DataLolos from './Lolos';
 class Pengumuman extends Component {
 
   renderTidakLolos = () => {
-    const { dataUser } = this.props
+    const { dataUser } = this.props;
 
     return (<div style={{ fontWeight: 'bold' }}>
       <div>Name: {dataUser.Identity.name}</div>
@@ -15,22 +15,22 @@ class Pengumuman extends Component {
   }
 
   renderLolos = (jalurs) => {
-    return (<Fragment>
-      <div>Name: {jalurs[0]['Nama Capes']}</div>
+    const { dataUser } = this.props;
+
+    return (<div style={{ fontWeight: 'bold' }}>
+      <p>Name: {dataUser.Identity.name}</p>
       {
         jalurs.map((jalur, index) => {
           return <div>Jalur {index+1}: {jalur.Jalur}</div>
         })
       }
-      <div>Selamat, kamu dinyatakan lolos seleksi tahap 1 Pelatnas FIM 21 dan berkesempatan untuk melanjutkan proses ke tahap selanjutnya (interview).</div>
-      <div>Seleksi tahap kedua akan dilangsungkan selama tanggal 7-13 September 2019. Panitia akan segera menghubungi kamu untuk penyesuaian jadwal interview. Jadi pastikan ponselmu selalu aktif dan tetap semangat. Semoga berhasil!</div>
-    </Fragment>)
+      <p style={{ marginTop: '10px' }} >Selamat, kamu dinyatakan lolos seleksi tahap 1 Pelatnas FIM 21 dan berkesempatan untuk melanjutkan proses ke tahap selanjutnya (interview).</p>
+      <p>Seleksi tahap kedua akan dilangsungkan selama tanggal 7-13 September 2019. Panitia akan segera menghubungi kamu untuk penyesuaian jadwal interview. Jadi pastikan ponselmu selalu aktif dan tetap semangat. Semoga berhasil!</p>
+    </div>)
   }
 
   render() {
     const { dataUser } = this.props
-    console.log("dataUser: ", dataUser)
-    console.log("DataLolos: ", DataLolos)
 
     const dataFiltered = DataLolos.filter((data) => {
       if (data['E-mail'] !== '0') {
@@ -40,34 +40,12 @@ class Pengumuman extends Component {
       }
     })
 
-    console.log("dataFiltered: ", dataFiltered)
-
     const isLolos = dataFiltered.length > 0;
-
-    console.log("isLolos: ", isLolos)
-
-    const tidakLolos = [
-      {
-        "Nama Capes": "Muhammad Nasar K",
-        "E-mail": "muhammadnasar22@gmail.com",
-        "Jalur": "NEXT - GENERATION"
-      },
-      {
-        "Nama Capes": "Muhammad Fachrizal",
-        "E-mail": "mhdfachrizal@gmail.com",
-        "Jalur": "NEXT - GENERATION"
-      },
-      {
-        "Nama Capes": "Muhammad Hafid",
-        "E-mail": "muhammad.hafid744@gmail.com",
-        "Jalur": "NEXT - GENERATION"
-      },
-    ]
 
     return (<Fragment>
       <h1>Pengumuman</h1>
       {
-        isLolos ? "LOLOS!!" : this.renderTidakLolos()
+        isLolos ? this.renderLolos(dataFiltered) : this.renderTidakLolos()
       }
     </Fragment>)
   }
