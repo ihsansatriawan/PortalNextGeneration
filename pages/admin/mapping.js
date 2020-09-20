90198fb67cd6dbff47aa4b8696c51dc36c0681e2
baguskah <dwiutamabagus@gmail.com>
2020-09-20 15:08:41 +0700

    add mapping status

--- pages/admin/mapping.js
+++ pages/admin/mapping.js
@@ -30,7 +30,6 @@ import ExcelJS from 'exceljs'
 import { saveAs } from 'file-saver'
 import WrapperStatistic from 'antd/lib/statistic/Statistic';
 
-const { Option, OptGroup } = Select;
 const CheckboxGroup = Checkbox.Group;
 
 const PendaftarPage = (props) => {
@@ -50,7 +49,7 @@ const PendaftarPage = (props) => {
     const [recruiters, setRecruiters] = useState([]);
 
     // Search state
-
+    
 
     useEffect(() => {
         fetchSemuaPendaftar()
@@ -81,7 +80,6 @@ const PendaftarPage = (props) => {
             regional: 'Loading Data...',
         },
     ])
-
 
     const [filteredParticipant, setFilteredParticipant] = useState(null)
 
@@ -162,52 +160,6 @@ const PendaftarPage = (props) => {
                     {/* <a onClick={(e) => onNilaiSekarang(e, record.ktp, record.TunnelId)}>Nilai Sekarang</a> */}
                 </span>
             ),
-        },
-        {
-            title: 'Status Penerimaan',
-            key: 'status_accept',
-            render: (text, record) => {
-                let statusString = null;
-                switch (record.status_accept) {
-                    case 0:
-                        statusString = "0. Seleksi Administrasi"
-                        break;
-                    case 1:
-                        statusString = "1. Berhasil ke Tahap Interview"
-                        break;
-                    case 100:
-                        statusString = "2. Lolos FIM 22"
-                        break;
-                    case 999999:
-                        statusString = "3. Belum Lolos FIM 22"
-                        break;
-                    default:
-                        break;
-                }
-
-                return <span>
-                    <div>
-                        {statusString}
-                    </div>
-                    <Select
-                        showSearch
-                        style={{ width: 200 }}
-                        placeholder="Select Status"
-                        optionFilterProp="children"
-                        onChange={onChangeStatusAccept}
-                        allowClear
-                        onChange={(e) => onChangeStatusAccept(e, record.ktpNumber)}
-                    >
-                        <Option value="0">Seleksi Administrasi</Option>
-                        <Option value="1">Berhasil ke Tahap Interview</Option>
-                        <Option value="999999">Belum Lolos FIM 22</Option>
-
-                        <OptGroup label="Diterima">
-                            <Option value="22">Lolos FIM 22</Option>
-                        </OptGroup>
-                    </Select>
-                </span>
-            }
         }
     ]
 
@@ -323,6 +275,7 @@ const PendaftarPage = (props) => {
             })
 
             const status = (response.data.status || false)
+            console.log(response.data.data)
             if (!status) {
                 message.error(response.data.message)
             } else {
@@ -445,51 +398,17 @@ const PendaftarPage = (props) => {
 
 
     const searchHandling = value => {
-
+        
         const filterTable = allParticipants.filter(o =>
-            Object.keys(o).some(k =>
-                String(o[k])
-                    .toLowerCase()
-                    .includes(value.toLowerCase())
-            )
+          Object.keys(o).some(k =>
+            String(o[k])
+              .toLowerCase()
+              .includes(value.toLowerCase())
+          )
         );
-
+    
         setFilteredParticipant(filterTable);
-    };
-
-    const onChangeStatusAccept = async (value, ktpNumber) => {
-
-        if (value) {
-            const { cookieLogin, refetchStep } = props;
-
-            try {
-                const response = await fetch({
-                    url: '/participant/update-status-accept',
-                    method: 'post',
-                    headers: {
-                        'Authorization': `Bearer ${cookieLogin}`
-                    }, data: {
-                        value: value,
-                        ktpNumber: ktpNumber
-                    }
-                })
-
-                const status = (response.data.status || false)
-
-                if (!status) {
-                    message.error(response.data.message)
-                } else {
-                    fetchSemuaPendaftar();
-                    setFilteredParticipant(null)
-                    message.success(response.data.message)
-                }
-
-            } catch (error) {
-                message.error("Server Error")
-                setIsLoadingRecruiter(false)
-            }
-        }
-    }
+      };
 
     return (
         <AdminPage>
